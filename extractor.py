1ed07d8b2c1d6514a6a56a684ee945e915cae345
qinlong <https://github.com/wenqinlong/w_b_transfer.git>
2019-05-05 10:53:16 -0400

    finised extractor and tfrecord_gfile

--- extractor.py
+++ extractor.py
@@ -27,37 +27,39 @@ class TFRecordExtractor:
 
         sample = tf.parse_single_example(tfrecord, features)
         # print(sample['image'])
-        # image = tf.decode_raw(sample['image'], out_type=tf.int64)   # if not use gfile
-        image = tf.image.decode_png(sample['image'], channels=0, dtype=tf.uint8,)
+        image = tf.decode_raw(sample['image'], out_type=tf.int64)   # if not use gfile
+        # image = tf.image.decode_png(sample['image'], channels=0, dtype=tf.uint8,)
         img_shape = tf.stack([sample['rows'], sample['cols'], 1], axis=0)
         label = sample['label']
         filename = sample['filename']
+
         return [image, label, filename, img_shape]
 
     def extract_image(self):
         dataset = tf.data.TFRecordDataset([self.tfrecord_file])
         dataset = dataset.map(self._extract_fn)
-        dataset = dataset.shuffle(buffer_size=100000)   # need to make sure
+        dataset = dataset.shuffle(buffer_size=38916)   # need to make sure
         dataset = dataset.repeat(self.epoch)
-        dataset = dataset.batch(self.batch_size)
-        dataset = dataset.prefetch(buffer_size=100000)  # to make sure
+        dataset = dataset.batch(self.batch_size, drop_remainder=True)
+        dataset = dataset.prefetch(buffer_size=self.batch_size)  # to make sure
         iterator = dataset.make_one_shot_iterator()
-        next_image = iterator.get_next()
 
-        return next_image
+        return iterator
 
     def show_images(self):
 
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
+            iterator = self.extract_image()
+            for i in range(self.epoch * (1080 // self.batch_size)):  # If the range is not right, it will raise OutOfRangeError
 
-            image_data = self.extract_image()
-            img_data = sess.run(image_data)
-            print(img_data[0], img_data[1], image_data[2], image_data[3])
+                next_image = iterator.get_next()
 
+                img_data = sess.run(next_image)
+                print(img_data[2])
 
 
 if __name__ == '__main__':
     path = '/home/qinlong/PycharmProjects/NEU/Holography'
-    t = TFRecordExtractor(path, './images.tfrecord', 64, 1)
+    t = TFRecordExtractor(path, './images.tfrecord', 9, 2)
     t.show_images()
--- tfrecord.py
+++ tfrecord.py
@@ -12,23 +12,25 @@ class GenerateTFRecord:
  
     def convert_image_folder(self, img_folder, tfrecord_file_name):
         img_folder_paths = os.listdir(img_folder)
+        assert len(img_folder_paths) == 36
+
         img_folder_paths.sort() # 0-Z
         for folder in img_folder_paths:
             folder_path = os.path.join(img_folder, folder)  # get the path of folder which is images in
             imgs = sorted(os.listdir(folder_path))
             imgs = [os.path.abspath(os.path.join(folder_path, i)) for i in imgs]
+            assert len(imgs) == 1080
 
             with tf.python_io.TFRecordWriter(tfrecord_file_name) as writer:
                 for img in imgs:
-                    print(img)
+                    # print(img)
                     example = self._convert_img(img)
                     writer.write(example.SerializeToString())
 
     def _convert_img(self, img_path):
-        label = self._get_label_with_filename(img_path)
-        img = Image.open(img_path)
-        # assert img.dtype == 'uint8'
+        label = self._get_label_with_filename(img_path)   # label: [0, ..., 35]
 
+        img = Image.open(img_path)
         img_int = np.array(img, dtype=np.int64)
         assert img_int.dtype == np.int64
         img_shape = img_int.shape
@@ -37,10 +39,8 @@ class GenerateTFRecord:
         # convert img to string data
         img_str = img_int.tobytes()
 
-
         # get filename
         filename = os.path.basename(img_path)   # return the file name without path information
-
         example = tf.train.Example(features=tf.train.Features(feature={
             'filename': tf.train.Feature(bytes_list=tf.train.BytesList(value=[filename.encode('utf-8')])),
             'rows': tf.train.Feature(int64_list=tf.train.Int64List(value=[img_shape[0]])),
@@ -52,14 +52,50 @@ class GenerateTFRecord:
 
     def _get_label_with_filename(self, filename):
         basename = os.path.basename(filename).split('.')[0]
-        basename = basename.split('_')[2]
+        basename = basename.split('_')[2]      # [0-9, A-Z]
         return self.labels[basename]
 
 
 if  __name__ == '__main__':
-    digits = list(string.digits)
-    letter_uppercase = list(string.ascii_uppercase)
-    keys = digits + letter_uppercase
-    labels = {k: v for v, k in enumerate(keys)}
-    t = GenerateTFRecord(labels)
-    t.convert_image_folder('hologram_image', 'images.tfrecord')
+    # digits = list(string.digits)
+    # letter_uppercase = list(string.ascii_uppercase)
+    # keys = digits + letter_uppercase
+    # labels = {k: v for v, k in enumerate(keys)}
+    # print(labels)
+    # t = GenerateTFRecord(labels)
+    # t.convert_image_folder('hologram_image', 'images.tfrecord')
+
+    def read_and_decode(filename_queue):
+        reader = tf.TFRecordReader()
+        _, serialized_example = reader.read(filename_queue)
+        features = tf.parse_single_example(
+            serialized_example,
+            # Defaults are not specified since both keys are required.
+            features={
+                'filename': tf.FixedLenFeature([], tf.string),
+                'rows': tf.FixedLenFeature([], tf.int64),
+                'cols': tf.FixedLenFeature([], tf.int64),
+                'image': tf.FixedLenFeature([], tf.string),
+                'label': tf.FixedLenFeature([], tf.int64)
+            })
+        image = tf.decode_raw(features['image'], tf.uint8)
+        label = tf.cast(features['label'], tf.int32)
+
+        return image, label
+
+    def get_all_records(FILE):
+        with tf.Session() as sess:
+            filename_queue = tf.train.string_input_producer([FILE])
+            image, label = read_and_decode(filename_queue)
+            init_op = tf.global_variables_initializer()
+            sess.run(init_op)
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(coord=coord)
+            for i in range(2053):
+                example, l = sess.run([image, label])
+                print(l)
+            coord.request_stop()
+            coord.join(threads)
+
+
+    get_all_records('images.tfrecord')
